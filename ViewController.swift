71e033b3028700e1a4fea926baa54eba6cf483ea
WilmerU7899 <wilmeru7899@hstat.org>
2021-06-14 22:42:34 -0400

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Sleep and Obesity
+//
+//  Created by Wilmer Uguna.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+//    @IBOutlet var picker: UIPickerView!
+    
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        picker.dataSource = self
+//        picker.delegate = self
+        
+    }
+
+
+}
+
+
+/* extension ViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data.count
+    }
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[row]
+    }
+ } /**/*/
